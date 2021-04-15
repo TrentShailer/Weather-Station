b75c95d781ac2bded76fe6fcdbf1449c325a3836
Trent Shailer <trent.shailer@gmail.com>
2021-04-15 13:17:07 +1200

    Material UI and other dependancies setup

--- Web-Server/front-end/src/App.js
+++ Web-Server/front-end/src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
-	return <div></div>;
+	const darkTheme = createMuiTheme({
+		palette: {
+			type: "dark",
+		},
+	});
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Button variant="contained" color="primary">
+				Hello World
+			</Button>
+		</ThemeProvider>
+	);
 }
-
 export default App;
