928bbeba3652c832e26da24ee3725d513597fa65
Trent Shailer <trent.shailer@gmail.com>
2021-04-15 21:29:11 +1200

    Full front end functionality

--- Web-Server/front-end/src/App.js
+++ Web-Server/front-end/src/App.js
@@ -1,19 +1,130 @@
 import React from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, Dialog, DialogContent, DialogTitle, Tooltip } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import StatCard from "./StatCard.js";
 import Footer from "./Footer.js";
 import axios from "axios";
 import { withSnackbar } from "notistack";
 
+import {
+	faThermometerHalf,
+	faCloudShowersHeavy,
+	faCloudDownloadAlt,
+	faSun,
+	faLightbulb,
+	faWind,
+	faTint,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { ResponsiveLine } from "@nivo/line";
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			values: { temperature: 0, humidity: 0, windSpeed: 0, uv: 0, light: 0, pressure: 0 },
+			values: {
+				temperature: 0,
+				humidity: 0,
+				windSpeed: 0,
+				uv: 0,
+				light: 0,
+				pressure: 0,
+				weather: "",
+			},
 			updated: "2021-04-15 5:50pm NZT",
+			openGraph: false,
+			theme: {
+				background: "#424242",
+				textColor: "#e0e0e0",
+				fontSize: 12,
+				axis: {
+					domain: {
+						line: {
+							stroke: "#777777",
+							strokeWidth: 2,
+						},
+					},
+					ticks: {
+						line: {
+							stroke: "#777777",
+							strokeWidth: 1,
+						},
+					},
+				},
+				grid: {
+					line: {
+						stroke: "#dddddd",
+						strokeWidth: 0.5,
+					},
+				},
+				tooltip: {
+					container: {
+						background: "#212121",
+					},
+				},
+			},
+			data: [
+				{
+					id: "data",
+					color: "",
+					data: [
+						{
+							x: "2021-04-03",
+							y: 160,
+						},
+						{
+							x: "2021-04-04",
+							y: 116,
+						},
+						{
+							x: "2021-04-05",
+							y: 300,
+						},
+						{
+							x: "2021-04-06",
+							y: 143,
+						},
+						{
+							x: "2021-04-07",
+							y: 207,
+						},
+						{
+							x: "2021-04-08",
+							y: 121,
+						},
+						{
+							x: "2021-04-09",
+							y: 84,
+						},
+						{
+							x: "2021-04-10",
+							y: 179,
+						},
+						{
+							x: "2021-04-11",
+							y: 161,
+						},
+						{
+							x: "2021-04-12",
+							y: 233,
+						},
+						{
+							x: "2021-04-13",
+							y: 6,
+						},
+						{
+							x: "2021-04-14",
+							y: 242,
+						},
+					],
+				},
+			],
+			dataValue: "Temperature",
+			dataUnit: "°C",
 		};
 		this.GetData = this.GetData.bind(this);
+		this.CloseGraph = this.CloseGraph.bind(this);
+		this.OpenGraph = this.OpenGraph.bind(this);
 	}
 
 	componentDidMount() {
@@ -31,35 +142,152 @@ class App extends React.Component {
 			});
 	};
 
+	CloseGraph = () => {
+		this.setState({ openGraph: false });
+	};
+
+	OpenGraph = (unit, dataValue) => {
+		axios
+			.post(`/Get${dataValue}`)
+			.then((response) => {
+				this.setState({
+					unit: unit,
+					dataValue: dataValue,
+					data: response.data.data,
+					openGraph: true,
+				});
+			})
+			.catch((error) => {
+				this.props.enqueueSnackbar("Failed to fetch data.", { variant: "error" });
+			});
+	};
+
 	render() {
 		return (
 			<div>
 				<CssBaseline />
+				<Dialog maxWidth="xl" fullScreen open={this.state.openGraph} onClose={this.CloseGraph}>
+					<DialogTitle>{this.state.dataValue}</DialogTitle>
+					<DialogContent>
+						<ResponsiveLine
+							theme={this.state.theme}
+							data={this.state.data}
+							margin={{ top: 50, right: 60, bottom: 100, left: 100 }}
+							xScale={{ type: "point" }}
+							yScale={{
+								type: "linear",
+								min: 0,
+								max: "auto",
+								stacked: false,
+								reverse: false,
+							}}
+							yFormat={(value) => `${Number(value)}${this.state.dataUnit}`}
+							curve="monotoneX"
+							axisTop={null}
+							axisRight={null}
+							axisBottom={{
+								orient: "bottom",
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: -45,
+								legend: "Date",
+								legendOffset: 75,
+								legendPosition: "middle",
+							}}
+							axisLeft={{
+								orient: "left",
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: 0,
+								legend: this.state.dataValue,
+								legendOffset: -75,
+								legendPosition: "middle",
+								format: (value) => `${Number(value)}${this.state.dataUnit}`,
+							}}
+							colors={{ scheme: "accent" }}
+							pointSize={10}
+							pointColor={{ from: "color", modifiers: [] }}
+							pointBorderColor={{ from: "serieColor", modifiers: [] }}
+							pointLabelYOffset={-12}
+							enableArea={true}
+							useMesh={true}
+						/>
+					</DialogContent>
+				</Dialog>
 				<Container style={{ marginTop: 50 }}>
-					<Grid container style={{ flexGrow: 1 }} spacing={10}>
+					<Grid container style={{ flexGrow: 1 }} spacing={8}>
 						<Grid item xs={12}>
 							<Grid container justify="center" spacing={10}>
 								<Grid item>
-									<StatCard title="Temperature" value={this.state.values.temperature} unit="°C" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faThermometerHalf}
+										title="Temperature"
+										value={this.state.values.temperature}
+										unit="°C"
+									/>
 								</Grid>
 								<Grid item>
-									<StatCard title="Humidity" value={this.state.values.humidity} unit="%" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faTint}
+										title="Humidity"
+										value={this.state.values.humidity}
+										unit="%"
+									/>
 								</Grid>
 								<Grid item>
-									<StatCard title="Wind Speed" value={this.state.values.windSpeed} unit="km/h" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faCloudShowersHeavy}
+										title="Weather"
+										value={this.state.values.weather}
+										unit=""
+									/>
 								</Grid>
 							</Grid>
 						</Grid>
 						<Grid item xs={12}>
 							<Grid container justify="center" spacing={10}>
 								<Grid item>
-									<StatCard title="UV Index" value={this.state.values.uv} unit="" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faWind}
+										title="Wind Speed"
+										value={this.state.values.windSpeed}
+										unit=" km/h"
+									/>
 								</Grid>
 								<Grid item>
-									<StatCard title="Light Intensity" value={this.state.values.light} unit="Lux" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faLightbulb}
+										title="Light Intensity"
+										value={this.state.values.light}
+										unit=" Lux"
+									/>
 								</Grid>
 								<Grid item>
-									<StatCard title="Air Pressure" value={this.state.values.pressure} unit="hPa" />
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faSun}
+										title="UV Index"
+										value={this.state.values.uv}
+										unit=""
+									/>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12}>
+							<Grid container justify="center" spacing={10}>
+								<Grid item>
+									<StatCard
+										OpenGraph={this.OpenGraph}
+										icon={faCloudDownloadAlt}
+										title="Air Pressure"
+										value={this.state.values.pressure}
+										unit=" hPa"
+									/>
 								</Grid>
 							</Grid>
 						</Grid>
--- Web-Server/front-end/src/StatCard.js
+++ Web-Server/front-end/src/StatCard.js
@@ -10,19 +10,28 @@ import {
 } from "@material-ui/core";
 import TimelineIcon from "@material-ui/icons/Timeline";
 import { grey } from "@material-ui/core/colors";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class StatCard extends React.Component {
 	render() {
 		return (
 			<Card style={{ width: 250, height: 250 }}>
-				<CardContent>
-					<Typography color="textSecondary" variant="h5" gutterBottom>
-						{this.props.title}
-					</Typography>
+				<CardContent style={{ height: 185 }}>
+					<Grid justify="space-between" alignItems="center" container>
+						<Grid item>
+							<Typography color="textSecondary" variant="h5">
+								{this.props.title}
+							</Typography>
+						</Grid>
+						<Grid item>
+							<FontAwesomeIcon color={grey[300]} size="lg" icon={this.props.icon} />
+						</Grid>
+					</Grid>
+
 					<Container style={{ textAlign: "center", marginTop: 50 }}>
 						<Grid justify="center" alignItems="baseline" container>
 							<Grid item>
-								<Typography variant="h3" gutterBottom>
+								<Typography variant={this.props.title !== "Weather" ? "h3" : "h5"} gutterBottom>
 									{this.props.value}
 								</Typography>
 							</Grid>
@@ -35,9 +44,15 @@ class StatCard extends React.Component {
 					</Container>
 				</CardContent>
 				<CardActions>
-					<IconButton>
-						<TimelineIcon style={{ color: grey[500] }} />
-					</IconButton>
+					{this.props.title !== "Weather" ? (
+						<IconButton
+							onClick={() => {
+								this.props.OpenGraph(this.props.unit, this.props.title);
+							}}
+						>
+							<TimelineIcon style={{ color: grey[400] }} />
+						</IconButton>
+					) : null}
 				</CardActions>
 			</Card>
 		);
