0612fab2cf9b809084e28ed8e063c721283845de
Trent Shailer <trent.shailer@gmail.com>
2021-04-17 20:25:18 +1200

    Added loop

--- Pi/RainSensor.py
+++ Pi/RainSensor.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as io
+import time
 
 water_sensor = 18
 io.setmode(io.BCM)
 io.setup(water_sensor, io.IN)
 
-print(io.input(water_sensor))
+while True:
+    print(io.input(water_sensor))
+    time.sleep(0.5)
