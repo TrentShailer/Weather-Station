edfa070d629a40aef62eb5435aed221bcefd375e
Trent Shailer <trent.shailer@gmail.com>
2021-04-18 21:21:17 +1200

    changed APDS lib

--- Pi/apds9930.py
+++ Pi/apds9930.py
@@ -1,5 +1,11 @@
-from apds9930 import APDS9930
+from machine import Pin, I2C
+import time
+import APDS9930
 
-a = APDS9930(1)
+i2c = I2C(sda=Pin(5), scl=Pin(4))
 
-print(a.ambient_light)
+apds = APDS9930.APDS9930(i2c)
+
+while True:
+    time.sleep_ms(500)
+    print(apds.getALS())
