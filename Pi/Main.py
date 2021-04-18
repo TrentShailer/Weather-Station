f9c0f888c6c89973c820f9b348ff02b2050f8b28
Trent Shailer <trent.shailer@gmail.com>
2021-04-18 18:59:50 +1200

    Main program

---
+++ Pi/Main.py
@@ -0,0 +1,23 @@
+import adafruit_bme280
+import adafruit_veml7700
+import adafruit_veml6075
+import board
+import digitalio
+import busio
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+veml = adafruit_veml6075.VEML6075(i2c, integration_time=100)
+veml7700 = adafruit_veml7700.VEML7700(i2c)
+
+bme280.sea_level_pressure = 1013.25
+
+while True:
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("UV Index:", round(veml.uv_index, 1))
+    print("Ambient light:", veml7700.light, " Lux")
+    time.sleep(2)
