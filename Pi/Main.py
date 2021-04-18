040d829c148f15e1796073989fd2a065fa94a1a9
Trent Shailer <trent.shailer@gmail.com>
2021-04-18 19:34:46 +1200

    Proper addresses

--- Pi/Main.py
+++ Pi/Main.py
@@ -9,8 +9,8 @@ import time
 i2c = busio.I2C(board.SCL, board.SDA)
 
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
-veml = adafruit_veml6075.VEML6075(i2c, integration_time=100)
-veml7700 = adafruit_veml7700.VEML7700(i2c)
+veml = adafruit_veml6075.VEML6075(i2c, address=0x10, integration_time=100)
+veml7700 = adafruit_veml7700.VEML7700(i2c, address=0x10)
 
 bme280.sea_level_pressure = 1013.25
 
