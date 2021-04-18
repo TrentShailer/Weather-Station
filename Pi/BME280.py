b0642f0323276b5623066ae09d7723501fbf8e56
Trent Shailer <trent.shailer@gmail.com>
2021-04-18 14:06:48 +1200

    updated BME code

--- Pi/BME280.py
+++ Pi/BME280.py
@@ -1,13 +1,22 @@
-import smbus
-import time
+import smbus2
+import bme280
 
-bus = smbus.SMBus(1)
+port = 1
 address = 0x76
+bus = smbus2.SMBus(port)
 
-def readValue():
-	number = bus.read_byte_data(address, 1)
-	return number
+calibration_params = bme280.load_calibration_params(bus, address)
 
-while True:
-	print(readValue())
-	time.sleep(0.5)
+# the sample method will take a single reading and return a
+# compensated_reading object
+data = bme280.sample(bus, address, calibration_params)
+
+# the compensated_reading class has the following attributes
+print(data.id)
+print(data.timestamp)
+print(data.temperature)
+print(data.pressure)
+print(data.humidity)
+
+# there is a handy string representation too
+print(data)
