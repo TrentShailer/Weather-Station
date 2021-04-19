5ccfa09c9fe10ce5e5ad74fe5d3063b95d56054e
Trent Shailer <trent.shailer@gmail.com>
2021-04-19 12:36:09 +1200

    cleaned up python code

--- Pi/Main.py
+++ Pi/Main.py
@@ -6,6 +6,12 @@ import busio
 import time
 import requests
 from Secret import secret
+import RPi.GPIO as io
+import time
+
+water_sensor = 18
+io.setmode(io.BCM)
+io.setup(water_sensor, io.IN)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -15,11 +21,19 @@ veml6075 = adafruit_veml6075.VEML6075(i2c, integration_time=100)
 bme280.sea_level_pressure = 1013.25
 
 while True:
-    print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.relative_humidity)
-    print("Pressure: %0.1f hPa" % bme280.pressure)
-    print("UV Index:", round(veml6075.uv_index, 1))
+    totalTemp, totalHumidity, totalPressure, totalUV_Index, totalRain = 0
+    for i in range(0, 5):
+        totalTemp += bme280.temperature
+        totalHumidity += bme280.relative_humidity
+        totalPressure += bme280.pressure
+        totalUV_Index += veml6075.uv_index
+        totalRain += 1 - io.input(water_sensor)
+        time.sleep(60)
+
+    avgTemp = round(totalTemp / 5, 1)
+    avgHumidity = round(totalHumidity / 5, 1)
+    avgPressure = round(totalPressure / 5, 1)
+    avgUV_Index = round(totalUV_Index / 5, 1)
 
     requests.post("http://192.168.9.101:3002/SaveData",
-                  data={'secret': secret(), 'temperature': round(bme280.temperature, 1), 'humidity': round(bme280.humidity, 1), 'pressure': round(bme280.pressure, 1), 'uv': round(veml6075.uv_index, 1), 'rain': 0, 'wind': 0})
-    time.sleep(5 * 60)
+                  data={'secret': secret(), 'temperature': avgTemp, 'humidity': avgHumidity, 'pressure': avgPressure, 'uv': avgUV_Index, 'rain': totalRain, 'wind': 0})
