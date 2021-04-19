fd6083f0829898b69db78fc2c3ef8b3ce3561d5c
Trent Shailer <trent.shailer@gmail.com>
2021-04-19 13:02:37 +1200

    info logging

--- Pi/Main.py
+++ Pi/Main.py
@@ -27,6 +27,11 @@ while True:
     totalUV_Index = 0
     totalRain = 0
     for i in range(0, 5):
+        print("Temperature: ", round(bme280.temperature, 1), "°C")
+        print("Humidity: ", round(bme280.relative_humidity, 1), "%")
+        print("Pressure: ", round(bme280.pressure, 1), " hpa")
+        print("UV Index: ", round(veml6075.uv_index, 1))
+        print("Rain: ", 1 - io.input(water_sensor), " min")
         totalTemp += bme280.temperature
         totalHumidity += bme280.relative_humidity
         totalPressure += bme280.pressure
@@ -38,6 +43,12 @@ while True:
     avgHumidity = round(totalHumidity / 5, 1)
     avgPressure = round(totalPressure / 5, 1)
     avgUV_Index = round(totalUV_Index / 5, 1)
+
+    print("Average Temperature: ", avgTemp, "°C")
+    print("Average Humidity: ", avgHumidity, "%")
+    print("Average Pressure: ", avgPressure, " hpa")
+    print("Average UV Index: ", avgUV_Index)
+    print("Rain: ", totalRain, " min(s)")
 
     requests.post("http://192.168.9.101:3002/SaveData",
                   data={'secret': secret(), 'temperature': avgTemp, 'humidity': avgHumidity, 'pressure': avgPressure, 'uv': avgUV_Index, 'rain': totalRain, 'wind': 0})
