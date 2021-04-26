450a3bf47d793b0e39b7473a83f4a5b49300dd4f
Trent Shailer <trent.shailer@gmail.com>
2021-04-26 15:39:57 +1200

    timed loop

--- Pi/basic_encoder.py
+++ Pi/basic_encoder.py
@@ -10,9 +10,12 @@ io.setup(b_phase, io.IN)
 while True:
     a_val = 1
     ticks = 0
+    millis = int(round(time.time() * 1000))
     for i in range(0, 1000):
         if a_val != io.input(a_phase):
             ticks += 1
         a_val = io.input(a_phase)
         time.sleep(0.001)
+
+    print("Time:", millis - int(round(time.time() * 1000)))
     print(ticks, "ticks per second")
