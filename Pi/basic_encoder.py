efe6a367414b102f54a02eec4df594426fb63c5d
Trent Shailer <trent.shailer@gmail.com>
2021-04-26 15:33:59 +1200

    changed loop speed

--- Pi/basic_encoder.py
+++ Pi/basic_encoder.py
@@ -10,9 +10,9 @@ io.setup(b_phase, io.IN)
 while True:
     a_val = 1
     ticks = 0
-    for i in range(0, 500):
+    for i in range(0, 100):
         if a_val != io.input(a_phase):
             ticks += 1
         a_val = io.input(a_phase)
-        time.sleep(0.002)
+        time.sleep(0.01)
     print(ticks, "ticks per second")
