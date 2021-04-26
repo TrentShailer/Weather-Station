5f4ae8f540e40105e8c039c6b92e7a697c62de99
Trent Shailer <trent.shailer@gmail.com>
2021-04-26 15:22:32 +1200

    encoder basics

---
+++ Pi/basic_encoder.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as io
+import time
+
+a_phase = 20
+b_phase = 21
+io.setmode(io.BCM)
+io.setup(a_phase, io.IN)
+io.setup(b_phase, io.IN)
+
+while True:
+    print(io.input(a_phase))
+    print(io.input(b_phase))
+    print("\n")
+    time.sleep_ms(20)
