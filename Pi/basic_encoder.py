2a8403695db71d961daa316c08401f2250c15d14
Trent Shailer <trent.shailer@gmail.com>
2021-04-26 15:52:54 +1200

    global updates

--- Pi/basic_encoder.py
+++ Pi/basic_encoder.py
@@ -13,6 +13,7 @@ position = 0
 
 
 def A():
+    global prev_A
     if prev_A == io.input(a_phase):
         position += 1
 
@@ -20,6 +21,7 @@ def A():
 
 
 def B():
+    global prev_B
     if prev_B == io.input(b_phase):
         position += 1
 
